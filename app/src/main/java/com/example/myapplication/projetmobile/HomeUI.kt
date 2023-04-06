0b70f339120cf81e246ce07da8432d192a0af9c0
mansour <mansourpouye@gmail.com>
2023-04-06 17:00:51 +0000

    icons commit

--- app/src/main/java/com/example/myapplication/projetmobile/HomeUI.kt
+++ app/src/main/java/com/example/myapplication/projetmobile/HomeUI.kt
@@ -1,22 +1,35 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.example.myapplication.projetmobile
 
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class HomeUI :ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,13 +44,17 @@ class HomeUI :ComponentActivity() {
         }
     }
 }
+const val colorPersonnel=0xFF1E88E5
+
+
 @Composable
-fun TopBarMenu(){
+fun TopBarMenu(onMenuCliked: () -> Unit){
     TopAppBar(
         navigationIcon = {
                          Icon(
                              imageVector = Icons.Default.Menu,
-                             contentDescription ="Menu"
+                             contentDescription ="Menu",
+                             modifier = Modifier.clickable(onClick =  onMenuCliked )
                          )
         },
         title = {
@@ -47,22 +64,90 @@ fun TopBarMenu(){
                 modifier = Modifier.padding(horizontal = 50.dp)
             )
         },
-        backgroundColor= Color(color = 0xFF1E88E5)
+        backgroundColor= Color(color = colorPersonnel)
 
     )
 }
+
+
 
 @Preview(showBackground =true )
 @Composable
 fun BottomBar(){
     BottomAppBar(
-        backgroundColor= Color(color = 0xFF1E88E5),
+        backgroundColor= Color(color = colorPersonnel),
         cutoutShape = MaterialTheme.shapes.small.copy(
         CornerSize(percent = 50)
     )
     ) {
 
-    Text(text = "Add", color = Color.White)
+    Surface {
+
+        Column(modifier = Modifier.background(Color(color = colorPersonnel) )) {
+            val iconRotations = remember {
+                listOf(
+                    mutableStateOf(false),
+                    mutableStateOf(false),
+                    mutableStateOf(false)
+                )
+            }
+            Row(horizontalArrangement = Arrangement.spacedBy(90.dp), modifier = Modifier.background(Color(color = colorPersonnel))) {
+                
+                Icon(
+                    imageVector =  Icons.Default.Home ,
+                    contentDescription ="",
+                    tint = Color.White,
+                    modifier= Modifier
+                        .background(Color(color = colorPersonnel))
+                        .width(40.dp)
+                        .clickable {
+                            iconRotations[0].value = !iconRotations[0].value
+                        }
+                        .rotate(if (iconRotations[0].value) 45f else 0f)
+                    ,
+                )
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription ="",
+                    modifier= Modifier
+                        .background(Color(color = colorPersonnel))
+                        .width(40.dp)
+                        .clickable {
+                            iconRotations[1].value = !iconRotations[1].value
+                        }
+                        .rotate(if (iconRotations[1].value) 45f else 0f)                   ,
+                    tint = Color.White,
+                )
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription ="",
+                    modifier= Modifier
+                        .background(Color(color = colorPersonnel))
+                        .width(40.dp)
+                        .clickable {
+                            iconRotations[2].value = !iconRotations[2].value
+                        }
+                        .rotate(if (iconRotations[2].value) 45f else 0f)
+                    ,
+                    tint = Color.White,
+                )
+            }
+            LaunchedEffect(iconRotations[0].value) {
+                delay(200) // Attendre une seconde
+                iconRotations[0].value = false // Réinitialiser la rotation
+            }
+
+            LaunchedEffect(iconRotations[1].value) {
+                delay(200) // Attendre une seconde
+                iconRotations[1].value = false // Réinitialiser la rotation
+            }
+
+            LaunchedEffect(iconRotations[2].value) {
+                delay(200) // Attendre une seconde
+                iconRotations[2].value = false // Réinitialiser la rotation
+            }
+        }
+    }
 
     }
 }
@@ -78,8 +163,8 @@ fun Drawer() {
     ) {
         // Repeat is a loop which
         // takes count as argument
-        repeat(5) { item ->
-            Text(text = "Item number $item", modifier = Modifier.padding(8.dp), color = Color.Black)
+        repeat(1) { item ->
+            Text(text = "Se Deconnecter $item", modifier = Modifier.padding(8.dp), color = Color.Black)
         }
     }
 }
@@ -94,7 +179,7 @@ fun Body() {
             .fillMaxSize()
             .background(Color.White)
     ) {
-        Text(text = "Aucun Projet", color = Color(0xFF1E88E5))
+        Text(text = "Empty", color = Color(0xFF1E88E5))
     }
 }
 
@@ -106,18 +191,22 @@ fun ScaffoldExample() {
     val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
 
     // Create a coroutine scope. Opening of
-    // Drawer and snackbar should happen in
     // background thread without blocking main thread
-
+    val  carotineScop = rememberCoroutineScope()
     // Scaffold Composable
     Scaffold(
 
         // pass the scaffold state
         scaffoldState = scaffoldState,
-
-        // pass the topbar we created
+        modifier = Modifier.fillMaxSize(),
         topBar = {
-            TopBarMenu()
+            TopBarMenu (
+                onMenuCliked = {
+                    carotineScop.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
         },
 
         // pass the bottomBar
@@ -136,14 +225,21 @@ fun ScaffoldExample() {
         floatingActionButton = {
             // Create a floating action button in
             // floatingActionButton parameter of scaffold
-            FloatingActionButton(
 
-                onClick = {
+            FloatingActionButton(
+                modifier = Modifier
+                    .border(2.dp,MaterialTheme.colors.secondary, CircleShape),
+                backgroundColor= Color(color = 0xFF1E88E5),
+
+                        onClick = {
 
                 }) {
                 // Simple Text inside FAB
-                Text(text = "X")
+                Text(text = "+")
             }
+
+
         }
     )
+
 }
